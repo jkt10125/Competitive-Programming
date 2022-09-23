1cd9ff8f42187a5c5fd22a2bbc0ff7d0bff3d8fb
jkt10125 <jkt10125@gmail.com>
2022-09-23 17:32:24 +0530

    added seni initial commit

---
+++ Problems/Nearest_Exit.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	int t;
+	cin >> t;
+	while(t--) {
+		int n;
+		cin >> n;
+		if(n > 50) cout<<"RIGHT";
+		else cout<<"LEFT";
+		cout<<endl;
+	}
+}
