449b3117377b2919c0b93c1427f290e98a514197
jkt10125 <jkt10125@gmail.com>
2022-10-03 11:17:10 +0530

    revised fft

--- Snippets/fft.cpp
+++ Snippets/fft.cpp
@@ -3,18 +3,14 @@
 using namespace std;
 
 #define int long long
-const int MAXN = 300005;
 
 /* PARTS OF CODE for fft taken from https://cp-algorithms.com/algebra/fft.html */
 
 
 const int mod = 998244353;
-const int root = 15311432; // which is basically 3 ^ 119 
+const int root = 15311432; // which is basically 3 ^ 119 % mod
 const int root_1 = 469870224;
 const int root_pw = (1 << 23);
-
-int fact[MAXN + 1], ifact[MAXN + 1], sum_pow[MAXN + 1];
-vector<int> P(MAXN); // this will be the first few terms of e^(x + (x^2)/2).
 
 int power(int a, int b, int mod=0) {
 	int pow = 1;
@@ -30,32 +26,16 @@ inline int inverse(int a) { // returns the modular inverse of
 	return power(a % mod, mod - 2, mod);
 }
 
-void init_fact() { // initializes fact[ ] and ifact[ ]
-	fact[0] = ifact[0] = 1;
-	for(int i = 1; i <= MAXN; ++i) {
-		fact[i] = fact[i - 1]  * i % mod;
-		ifact[i] = inverse(fact[i]);
-	}
-}
-
-int C(int n, int r) { // returns nCr in O(1) time
-	return (r > n || r < 0) ? 0 : (ifact[r] * ifact[n - r] % mod * fact[n] % mod);
-}
-
 // code for fft in O(nlogn)
 void fft(vector<int>& a, bool invert) {
 	int n = a.size();
 
 	/// this does the bit inversion 
-	for(int i = 1, j = 0; i < n; ++i) {
+	for(int i = 1, j = 0; i < n; i++) {
 		int bit = n >> 1;
-		for(; j & bit; bit >>= 1) {
-			j ^= bit;
-		}
+		for(; j & bit; bit >>= 1) j ^= bit;
 		j ^= bit;
-		if(i < j) {
-			swap(a[i], a[j]);
-		}
+		if(i < j) swap(a[i], a[j]);
 	}
 
 	for(int len = 2; len <= n; len <<= 1) {
@@ -82,7 +62,7 @@ void fft(vector<int>& a, bool invert) {
 	}
 }
 
-//multiplying two polynomials a and b using ntt in O(max(A, B)log(max(A, B))), where A, B are degrees of a, b respectively
+// a * b in O(nlogn)
 vector<int> multiply(vector<int> const &a, vector<int> const &b) {
 
 	vector<int> fa = a, fb = b;
@@ -90,13 +70,12 @@ vector<int> multiply(vector<int> const &a, vector<int> const &b) {
 	while(n < (int)a.size() + (int)b.size()) {
 		n <<= 1;
 	}
-
 	fa.resize(n);
 	fb.resize(n);
 
 	fft(fa, false);
 	fft(fb, false);
-	for(int i = 0; i < n; ++i) {
+	for(int i = 0; i < n; i++) {
 		fa[i] = fa[i] * fb[i] % mod;
 	}
 	fft(fa, true);
@@ -118,7 +97,4 @@ signed main() {
     for(int i=0; i<c.size(); i++) {
         cerr<<i<<" : "<<c[i]<<endl;
     }
-
-    vector<int> D = A;
-    for(auto it : D) cout<<it<<" ";
 }
