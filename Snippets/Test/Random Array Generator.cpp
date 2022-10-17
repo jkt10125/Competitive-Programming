7f2878ab626c32cf7b68673a4b2ad0f11a6ea79f
jkt10125 <jkt10125@gmail.com>
2022-10-17 12:46:58 +0530

    random array Generator

--- Snippets/Test/Random Array Generator.cpp
+++ Snippets/Test/Random Array Generator.cpp
@@ -1,9 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define int long long
+vector<int> rand(int n) {
+    vector<int> A(n);
+    for(int i=0; i<n; i++) A[i] = i + 1;
+    srand(time(NULL));
+    while(n) {
+        int idx = rand() % n;
+        swap(A[idx], A[n-1]);
+        n--;
+    }
+    return A;
+}
 
 signed main(int argc, char *argv[]) {
+    srand(time(NULL));
     
+    int n = atoi(argv[1]);
+
+    vector<int> A = rand(n);
+    for(auto it : A) cout << it << " ";
+    cout<<endl;
     return 0;
 }
