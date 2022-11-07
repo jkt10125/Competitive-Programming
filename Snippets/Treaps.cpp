119d93303207a0d99e74e06ea4469135c6a2c3e8
jkt10125 <jkt10125@gmail.com>
2022-11-07 23:31:34 +0530

    ...

--- Snippets/Treaps.cpp
+++ Snippets/Treaps.cpp
@@ -1,146 +1,163 @@
-mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-uniform_int_distribution<int> uid(0, INT_MAX);
+/*
+Reference : https://cp-algorithms.com/data_structures/treap.html
+huge thanks to anubhavdhar for helping me in writing this code 
+Instructions: 	
+	PRIORITIES SHOULD BE DISTINCT 
+		I don't know why, but for some reason t2 in reverse() becomes NULL if there is a collision in priority
+		Make sure you do this my generating a random permutation of the sequence {1, 2, .. , N + Q} maybe
+	
+	In the first line change the typedef from int to char if you want to use this on a string
+	Always remember to initialize the pointer to NULL before using it.
+*/
 
-template <typename T>
-class item {
-    public:
-    int priority, cnt;
-    T key, F, G;
-    bool rev;
-    item<T> *l, *r;
-    item(T k, int p = uid(rng)) {
-        cnt = 1;
-        rev = false;
-        F = G = 0;
-        l = r = NULL;
-        key = k;
-        priority = p;
-    }
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef int treap_data_type;
+struct item {
+	int prior, cnt;
+	treap_data_type value;
+	item *l, *r;
+	bool rev;
+	item () { }
+	item (treap_data_type value, int prior) : value(value), prior(prior), l(NULL), r(NULL), rev(0) { }
 };
 
-template <typename T>
-int cnt(item<T> *t) { return (!t) ? 0 : t->cnt; }
+typedef item * pitem;
 
-template <typename T>
-void update_cnt(item<T> *t) { if(t) t->cnt = 1 + cnt(t->l) + cnt(t->r); }
-
-template <typename T>
-T F(item<T> *t) { return (!t) ? 0 : t->F; }
-
-template <typename T>
-void f(item<T> *&t) { if(t) t->F = t->key + F(t->l) + F(t->r); }
-
-template <typename T> // splits into ( < key) & ( >= key)
-void split(item<T> *t, item<T> *&l, item<T> *&r, int key, int add = 0) {
-    push_rev(t); push_data(t);
-    if(!t) return void( l = r = 0 );
-    int cur_key = add + cnt(t->l);
-    if(key <= cur_key) split(t->l, l, t->l, key, add), r = t;
-    else split(t->r, t->r, r, key, add + 1 + cnt(t->l)), l = t;
-    update_cnt(l); f(l);
-    update_cnt(r); f(r);
+int cnt (pitem it) {
+	return it ? it->cnt : 0;
 }
 
-template <typename T>
-void merge(item<T> *&t, item<T> *l, item<T> *r) {
-    push_rev(l); push_data(l);
-    push_rev(r); push_data(r);
-    if(!l || !r) t = l ? l : r;
-    else if (l->priority > r->priority) merge(l->r, l->r, r), t = l;
-    else merge(r->l, l, r->l), t = r;
-    update_cnt(t); f(t);
+void upd_cnt (pitem it) {
+	if(!it) return;
+	it->cnt = cnt(it->l) + cnt(it->r) + 1;
 }
 
-template <typename T>
-void insert(item<T> *&t, int pos, item<T> *ky) {
-    item<T> *L, *R;
-    split(t, L, R, pos); // => ([0 ... pos-1]) & ([1 ... size-pos])
-    merge(L, L, ky); // => ([0 ... pos-1], ky) & ([1 ... size-pos])
-    merge(t, L, R); // => ([0 ... size+ky.size])
+void push (pitem it) {
+	if(it && it->rev) {
+		it->rev = false;
+		swap(it->l, it->r);
+		if(it->l) {
+			it->l->rev ^= true;
+		}
+		if(it->r) {
+			it->r->rev ^= true;
+		}
+	}
 }
 
-template <typename T>
-void erase(item<T> *&t, int l, int r) {
-    item<T> *L, *M, *R;
-    split(t, M, R, r+1);
-    split(M, L, M, l);
-    merge(t, L, R);
-    delete M;
+void merge (pitem &t, pitem l, pitem r) {
+	push(l);
+	push(r);
+	if (!l || !r) {
+		t = l ? l : r;
+	}
+	else if (l->prior > r->prior) {
+		merge(l->r, l->r, r), t = l;
+	}
+	else {
+		merge(r->l, l, r->l), t = r;
+	}
+	upd_cnt(t);
 }
 
-template <typename T>
-void push_rev(item<T> *&t) {
-    if(!t || !t->rev) return;
-    swap(t->l, t->r);
-    if(t->l) t->l->rev ^= 1;
-    if(t->r) t->r->rev ^= 1;
-    t->rev = 0;
+void split (pitem t, pitem &l, pitem &r, int key, int add = 0) { // apparantly splits (< T) and (>= T)
+	if (!t) return void( l = r = 0 );
+	push(t);
+	int cur_key = add + cnt(t->l);
+	if (key <= cur_key) {
+		split(t->l, l, t->l, key, add), r = t;
+	}
+	else {
+		split(t->r, t->r, r, key, add + 1 + cnt(t->l)), l = t;
+	}
+	upd_cnt(r);
+	upd_cnt(l);
+	upd_cnt(t);
 }
 
-template <typename T>
-void push_data(item<T> *&t) {
-    if(!t || !t->G) return;
-    if(t->l) { t->l->G = t->G; t->l->key = t->G; }
-    if(t->r) { t->r->G = t->G; t->r->key = t->G; }
-    t->G = 0;
+void insert (pitem &t, treap_data_type val, int prior, int i) { // inserts before the i-th index (This will become the i-th index);
+	pitem t1, t2, it = new item(val, prior);
+	split(t, t1, t2, i);
+	merge(t1, t1, it);
+	merge(t, t1, t2);
 }
 
-template <typename T>
-void reverse(item<T> *&t, int l, int r) {
-    item<T> *L, *M, *R;
-    split(t, M, R, r+1);
-    split(M, L, M, l);
-    M->rev ^= 1;
-    merge(M, L, M);
-    merge(t, M, R);
+void reverse (pitem &t, int l, int r) {
+	pitem t1, t2, t3;
+	split(t, t1, t2, l);
+	split(t2, t2, t3, r-l+1);
+	t2->rev ^= true;
+	merge(t, t1, t2);
+	merge(t, t, t3);
 }
 
-template <typename T>
-void right_cyclic_shift(item<T> *&t, int l, int r, int qty) {
+void erase (pitem &t, int i){
+	pitem t1, t2, t3;
+	split(t, t1, t2, i);
+	split(t2, t2, t3, 1);
+	merge(t, t1, t3);
+	delete t2;
+}
+
+void append (pitem &t, treap_data_type val, int prior){
+	pitem it = new item(val, prior);
+	it->cnt = 1;
+	t ? merge(t, t, it) : (t = it, upd_cnt(t));
+}
+
+void right_cyclic_shift (pitem &t, int l, int r, int qty) {
     qty = qty % (r-l+1);
-    item<T> *L, *M, *R;
+    pitem L, M, R;
     split(t, M, R, r+1);
     split(M, L, M, l);
-    item<T> *a, *b;
+    pitem a, b;
     split(M, a, b, r-l+1-qty);
     merge(M, b, a);
     merge(M, L, M);
     merge(t, M, R);
 }
 
-template <typename T>
-void left_cyclic_shift(item<T> *&t, int l, int r, int qty) {
+void left_cyclic_shift (pitem &t, int l, int r, int qty) {
     qty = qty % (r-l+1);
     right_cyclic_shift(t, l, r, r-l+1-qty);
 }
 
-template <typename T>
-void range_update(item<T> *&t, int l, int r, int val) {
-    item<T> *L, *M, *R;
-    split(t, M, R, r+1);
-    split(M, L, M, l);
-    M->G = val;
-    M->key = val;
-    merge(M, L, M);
-    merge(t, M, R);
+void output (pitem t) {
+	if (!t) return;
+	push(t);
+	output(t->l);
+	cout << t->value << ' ';
+	output(t->r);
 }
 
-template <typename T>
-T query(item<T> *&t, int l, int r) {
-    item<T> *L, *M, *R;
-    split(t, M, R, r+1);
-    split(M, L, M, l);
-    T ans = F(M);
-    merge(M, L, M);
-    merge(t, M, R);
-    return ans;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+uniform_int_distribution<int> uid(0, INT_MAX);
+
+// generates a random priority array [0 ... n-1]
+vector<int> rand_perm(int n) {
+    vector<int> A(n);
+    for(int i=0; i<n; i++) A[i] = i;
+    while(n) {
+        int idx = uid(rng) % n;
+        swap(A[idx], A[n-1]);
+        n--;
+    }
+    return A;
 }
 
-template <typename T>
-void output(item<T> *t) {
-    if(!t) return;
-    push_rev(t); push_data(t);
-    output(t->l);
-    cout << t->key << " ";
-    output(t->r);
+int main() {
+	int n;
+	cin >> n;
+	vector<int> A = rand_perm(n);
+	pitem tr = nullptr;
+	for(int i=0; i<n; i++) {
+		int x;
+		cin >> x;
+		append(tr, x, A[i]);
+	}
+	for(auto it : A) cerr << it << ' ';
+	reverse(tr, 1, 5);
+	output(tr);
 }
