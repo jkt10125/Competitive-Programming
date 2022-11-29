1e1b54489f446862caa6165d7b890b3187ff397a
jkt10125 <jkt10125@gmail.com>
2022-11-29 10:45:24 +0530

    fixed bugs

--- Snippets/Min Max Queue.cpp
+++ Snippets/Min Max Queue.cpp
@@ -1,59 +1,72 @@
-#define T int
+#include <bits/stdc++.h>
+using namespace std;
+
 class MinMaxQUEUE {
-	vector<pair<T, pair<T, T>>> push_stack, pop_stack;
-	void stack_transfer() {
-		while(push_stack.size() != 0) {
-			T mn, mx;
-			mn = mx = push_stack.back().first;
-			mn = (pop_stack.size() == 0) ? mn : std::min(mn, pop_stack.back().second.first);
-			mx = (pop_stack.size() == 0) ? mx : std::max(mx, pop_stack.back().second.second);
-			pop_stack.push_back({push_stack.back().first, {mn, mx}});
-			push_stack.pop_back();
+	vector<array<int, 3>> push_st, pop_st;
+	void transfer () {
+		while (!push_st.empty()) {
+			auto [a, b, c] = push_st.back();
+			if (!pop_st.empty()) {
+				b = std::min(b, pop_st.back()[1]);
+				c = std::max(c, pop_st.back()[2]);
+			}
+			pop_st.push_back({a, b, c});
+			push_st.pop_back();
 		}
 	}
-public:
-	int size() { return (push_stack.size() + pop_stack.size()); }
-	bool empty() { return (size() == 0); }
 
-	void push(T val) {
-		T mn = (push_stack.size() == 0) ? val : std::min(push_stack.back().second.first, val);
-		T mx = (push_stack.size() == 0) ? val : std::max(push_stack.back().second.second, val);
-		push_stack.push_back({val, {mn, mx}});
+	public:
+	int size () {
+		return push_st.size() + pop_st.size();
 	}
 
-	void pop() {
-		if(pop_stack.size() == 0) stack_transfer();
-		pop_stack.pop_back();
+	bool empty () {
+		return size ();
 	}
 
-	T min() {
-		if(pop_stack.size() == 0) return push_stack.back().second.first;
-		else if(push_stack.size() == 0) return pop_stack.back().second.first;
-		return std::min(push_stack.back().second.first, pop_stack.back().second.first);
-	}
-
-	T max() {
-		if(pop_stack.size() == 0) return push_stack.back().second.second;
-		else if(push_stack.size() == 0) return pop_stack.back().second.second;
-		return std::max(push_stack.back().second.second, pop_stack.back().second.second);
-	}
-
-	T front() {
-		if(pop_stack.size() == 0) stack_transfer();
-		return pop_stack.back().first;
-	}
-
-
-	friend ostream& operator<<(ostream &os, const MinMaxQUEUE &q) {
-		for(int i=q.pop_stack.size()-1; i>0; i--) os << q.pop_stack[i].first << " ";
-		if(q.pop_stack.size()) {
-			cout << q.pop_stack.front().first;
-			if(q.push_stack.size()) os << " ";
+	void push (int a) {
+		int b = a, c = a;
+		if (!push_st.empty()) {
+			b = std::min(b, push_st.back()[1]);
+			c = std::max(c, push_st.back()[2]);
 		}
-		for(int i=0; i<q.push_stack.size(); i++) {
-			os << q.push_stack[i].first;
-			if(i != q.push_stack.size()-1) os << " ";
+		push_st.push_back({a, b, c});
+	}
+
+	void pop () {
+		if (pop_st.empty()) transfer();
+		pop_st.pop_back();
+	}
+
+	int min () {
+		if (pop_st.empty()) return push_st.back()[1];
+		if (push_st.empty()) return pop_st.back()[1];
+		return std::min(push_st.back()[1], pop_st.back()[1]);
+	}
+
+	int max () {
+		if (pop_st.empty()) return push_st.back()[2];
+		if (push_st.empty()) return pop_st.back()[2];
+		return std::max(push_st.back()[2], pop_st.back()[2]);
+	}
+
+	int front () {
+		if (pop_st.empty()) transfer();
+		return pop_st.back()[0];
+	}
+
+	friend ostream & operator << (ostream &os, const MinMaxQUEUE &Q) {
+		int n = Q.pop_st.size();
+		for (int i = 0; i < n - 1; i++) os << Q.pop_st[i][0] << ' ';
+		if (!Q.pop_st.empty()) {
+			cout << Q.pop_st.back()[0];
+			if (!Q.push_st.empty()) {
+				cout << ' ';
+			}
 		}
+		n = Q.push_st.size();
+		for (int i = n - 1; i > 0; i--) os << Q.push_st[i][0] << ' ';
+		if (!Q.push_st.empty()) cout << Q.push_st.front()[0];
 		return os;
 	}
 };
--- Snippets/Min Max Stack.cpp
+++ Snippets/Min Max Stack.cpp
@@ -1,25 +1,44 @@
-#define T int
-class MinMaxSTACK {
-	vector<pair<T, pair<T, T>>> stack;
-public:
-	int size() { return stack.size(); }
-	bool empty() { return (size() == 0); }
-	
-	void push(T val) {
-		T mn = (size()) ? std::min(stack.back().second.first, val) : val;
-		T mx = (size()) ? std::max(stack.back().second.second, val) : val;
-		stack.push_back({val, {mn, mx}}); 
-	}
-	void pop() { stack.pop_back(); }
-	T max() { return stack.back().second.second; }
-	T min() { return stack.back().second.first; }
-	T top() { return stack.back().first; }
+#include <bits/stdc++.h>
+using namespace std;
 
-	friend ostream& operator<<(ostream &os, const MinMaxSTACK &v) {
-		for(auto it=v.stack.begin(); it!=v.stack.end(); it++) {
-			os << it->first;
-			if(it != --v.stack.end()) os << " ";
+class MinMaxSTACK {
+	vector<array<int, 3>> stack;
+	public:
+	int size () const {
+		return stack.size();
+	}
+	bool empty () const {
+		return size();
+	}
+	void push (int a) {
+		int b = a, c = a;
+		if (size()) {
+			b = std::min(b, stack.back()[1]);
+			c = std::max(c, stack.back()[2]);
 		}
+		stack.push_back({a, b, c});
+	}
+
+	void pop () {
+		stack.pop_back();
+	}
+
+	int max () {
+		return stack.back()[2];
+	}
+
+	int min () {
+		return stack.back()[1];
+	}
+
+	int top () {
+		return stack.back()[0];
+	}
+
+	friend ostream & operator << (ostream &os, const MinMaxSTACK &S) {
+		int n = S.stack.size();
+		for (int i = n - 1; i > 0; i--) os << S.stack[i][0] << ' ';
+		if (!S.empty()) os << S.stack.front()[0];
 		return os;
 	}
 };
