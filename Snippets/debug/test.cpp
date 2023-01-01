9464574245d5f02a9358f7fb153aa8a1c1d521ee
jkt10125 <jkt10125@gmail.com>
2023-01-01 17:32:30 +0530

    ...

--- Snippets/debug/test.cpp
+++ Snippets/debug/test.cpp
@@ -1,11 +1,12 @@
 #include "debug.hpp" 
 
 #include <bits/stdc++.h>
-using namespace std;
-
-#include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
 using namespace __gnu_pbds;
+
 template <typename T = int>
 using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
